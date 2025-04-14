c756bfef19e6fa26f508b5814a855ea9b9c9f00f
Rajveer Kaur <rajveer.kaur@learningworks.co.nz>
2025-04-14 14:17:04 +1200

    Initial commit for DjangoBasic branch

---
+++ hello/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# def index(request):
+#     return HttpResponse("Hello!")
+
+def index(request):
+    return render(request, 'hello/index.html')
+
+def brain (request):
+    return HttpResponse("Hello ,Brain!")
+
+def david(request):
+    return HttpResponse("Hello ,David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello ,{name.capitalize()}!")
