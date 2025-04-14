be2eb2bb3129223bf9757d82e65d8e9229ea168d
Rajveer Kaur <rajveer.kaur@learningworks.co.nz>
2025-04-14 14:28:57 +1200

    create the templates of greet fucnation

--- hello/views.py
+++ hello/views.py
@@ -15,5 +15,11 @@ def brain (request):
 def david(request):
     return HttpResponse("Hello ,David!")
 
+# def greet(request, name):
+#     return HttpResponse(f"Hello ,{name.capitalize()}!")
+
 def greet(request, name):
-    return HttpResponse(f"Hello ,{name.capitalize()}!")
+    return render(request, "hello/greet.html",{
+     "name":name.capitalize()
+
+    })
