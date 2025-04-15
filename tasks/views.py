705fead1c986ded3758da9c6f0c5deaa89b64892
Rajveer Kaur <rajveer.kaur@learningworks.co.nz>
2025-04-16 11:28:13 +1200

    modified the index request with session

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+
 from django import forms
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 
 # Create your views here.
@@ -12,25 +13,25 @@ class NewTaskForm(forms.Form):
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 
-def index (request):
+def index(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
-        return render(request, "tasks/index.html", {
-            "tasks": request.session["tasks"]})
-    
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
 
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            task = form.cleaned_data['task']
-            tasks.append(task)
-            return HttpResponseRedirect(reverse ("tasks:index"))
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
-            return render(request, "tasks/add.html",{
+            return render(request, "tasks/add.html", {
                 "form": form
-            })   
-        
-    return render(request,"tasks/add.html",{
-        "form":NewTaskForm()
-    })
+            })
+    else:
+        return render(request, "tasks/add.html", {
+            "form": NewTaskForm()
+        })
