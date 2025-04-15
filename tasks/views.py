26db9224bcc19d711ccc669fb6c5c7114a6acd1e
Rajveer Kaur <rajveer.kaur@learningworks.co.nz>
2025-04-16 08:35:10 +1200

    create add function

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,6 @@ tasks = ["foo","bar","baz"]
 def index (request):
     return render(request, 'tasks/index.html', {
         'tasks': tasks})
+
+def add(request):
+    return render(request,"tasks/add.html")
