22ae85b15b70243e7cb7ba510c2bb84e018c7a46
Rajveer Kaur <rajveer.kaur@learningworks.co.nz>
2025-04-16 10:47:41 +1200

    add the httpresponse and url reverse, after adding the item in list it take me to index page

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -20,6 +22,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data['task']
             tasks.append(task)
+            return HttpResponseRedirect(reverse ("tasks:index"))
         else:
             return render(request, "tasks/add.html",{
                 "form": form
